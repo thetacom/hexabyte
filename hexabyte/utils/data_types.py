9704c0b9c28baf3de2f15bfb620cd92bcdaaf974
Justin C <justin@thetacom.info>
2023-05-01 23:59:30 -0400

    Expands Selection class behavior and unit tests.

--- hexabyte/utils/data_types.py
+++ hexabyte/utils/data_types.py
@@ -1,7 +1,12 @@
 """Datatype classes module."""
+from __future__ import annotations
+
+from collections.abc import Iterable
 from dataclasses import dataclass
+from functools import total_ordering
 
 
+@total_ordering
 @dataclass
 class Selection:
     """Data selection."""
@@ -16,11 +21,25 @@ class Selection:
         if self.length < 1:
             raise ValueError("Length must be greater than 0.")
 
-    def __contains__(self, val: int) -> bool:
+    def __contains__(self, val: int | Selection) -> bool:
         """Determine if offset is in range."""
-        if not isinstance(val, int):
-            raise TypeError("Can only check if selection range contains an integer values.")
-        return self.offset <= val <= self.offset + self.length
+        if isinstance(val, int):
+            return self.offset <= val < self.offset + self.length
+        if isinstance(val, Selection):
+            return self.offset <= val.offset and self.end >= val.end
+        raise NotImplementedError
+
+    def __eq__(self, other):
+        """Determine if other item is equal to Selection instance."""
+        if not isinstance(other, Selection):
+            return NotImplemented
+        return (self.offset, self.length) == (other.offset, other.length)
+
+    def __lt__(self, other):
+        """Determine if other item is less than the Selection instance."""
+        if not isinstance(other, Selection):
+            return NotImplemented
+        return (self.offset, self.length) < (other.offset, other.length)
 
     def __len__(self) -> int:
         """Return selection length."""
@@ -40,3 +59,38 @@ class Selection:
     def start(self) -> int:
         """Return start of selection."""
         return self.offset
+
+    @classmethod
+    def reduce(cls, selections: Iterable[Selection]) -> Iterable[Selection]:
+        """Sort and merge selections where applicable."""
+        selections = sorted(selections)
+        reduced_selections: list[Selection] = []
+        prev_selection: Selection | None = None
+        for selection in selections:
+            # Capture first selection as previous selection
+            if prev_selection is None:
+                prev_selection = selection
+                continue
+
+            # Check for discard conditions
+            if prev_selection == selection or selection in prev_selection:
+                continue
+            if prev_selection in selection:
+                prev_selection = selection
+                continue
+
+            # Check for merge conditions
+            if selection.start >= prev_selection.start and selection.start <= prev_selection.after:
+                new_end = max([prev_selection.end, selection.end])
+                new_length = new_end - prev_selection.start + 1
+                prev_selection = Selection(prev_selection.start, new_length)
+                continue
+
+            # Items cannot be combined, move to next selection
+            reduced_selections.append(prev_selection)
+            prev_selection = selection
+
+        # Add last item if it exists
+        if prev_selection is not None:
+            reduced_selections.append(prev_selection)
+        return reduced_selections
--- tests/utils/test_data_types.py
+++ tests/utils/test_data_types.py
@@ -27,3 +27,126 @@ def test_selection_invalid_construction() -> None:
         Selection(-100)
     with pytest.raises(ValueError):
         Selection(100, -1)
+
+
+def test_selection_attributes() -> None:
+    """Test that all calculated attributes are correct."""
+    selection = Selection(0x100, 0x100)
+    assert selection.offset == 256
+    assert selection.length == 256
+    assert selection.end == 511
+    assert selection.after == 512
+
+
+def test_selection_comparisons() -> None:
+    """Test that rich comparisons behave correctly."""
+    selection1 = Selection(0, 32)
+    selection2 = Selection(0, 32)
+    selection3 = Selection(0, 64)
+    selection4 = Selection(64, 32)
+    selection5 = Selection(64, 64)
+    assert selection1 == selection2
+    assert selection1 != selection3
+    assert selection1 < selection3
+    assert selection1 < selection4
+    assert selection3 < selection4
+    assert selection5 > selection4
+    assert "abc" != selection1
+    with pytest.raises(TypeError):
+        assert "abc" < selection1  # type: ignore
+
+
+def test_selection_contains() -> None:
+    """Test selection contains behavior."""
+    selection1 = Selection(0, 32)
+    selection2 = Selection(0, 32)
+    selection3 = Selection(0, 64)
+    selection4 = Selection(64, 32)
+    selection5 = Selection(64, 64)
+    assert 0 in selection1
+    assert 31 in selection1
+    assert 32 not in selection1
+    assert selection2 in selection1
+    assert selection1 in selection3
+    assert selection1 not in selection4
+    assert selection3 not in selection1
+    assert selection2 in selection3
+    assert selection4 in selection5
+    assert selection5 not in selection4
+    with pytest.raises(NotImplementedError):
+        assert "abc" in selection1  # type: ignore
+
+
+def test_selection_reduce1() -> None:
+    """Test reduce of empty set and single item."""
+    before: list[Selection] = []
+    after: list[Selection] = []
+    selections = Selection.reduce(before)
+    assert selections == after
+    before = [Selection(0, 64)]
+    after = [Selection(0, 64)]
+    selections = Selection.reduce(before)
+    assert selections == after
+
+
+def test_selection_reduce2() -> None:
+    """Test reduce of matching selections."""
+    before = [Selection(0, 64), Selection(0, 64)]
+    after = [Selection(0, 64)]
+    selections = Selection.reduce(before)
+    assert selections == after
+
+
+def test_selection_reduce3() -> None:
+    """Test reduce for nested selections."""
+    before = [Selection(0, 32), Selection(0, 64)]
+    after = [Selection(0, 64)]
+    selections = Selection.reduce(before)
+    assert selections == after
+
+
+def test_selection_reduce4() -> None:
+    """Test reduce for nested selections."""
+    before = [Selection(0, 64), Selection(0, 32)]
+    after = [Selection(0, 64)]
+    selections = Selection.reduce(before)
+    assert selections == after
+
+    # Same test but ordered differently
+    before.reverse()
+    selections = Selection.reduce(before)
+    assert selections == after
+
+
+def test_selection_reduce5() -> None:
+    """Test reduce of adjacent selections."""
+    before = [Selection(0, 32), Selection(32, 32)]
+    after = [Selection(0, 64)]
+    selections = Selection.reduce(before)
+    assert selections == after
+
+
+def test_selection_reduce6() -> None:
+    """Test reduce of overlapping selections."""
+    before = [Selection(0, 32), Selection(16, 32)]
+    after = [Selection(0, 48)]
+    selections = Selection.reduce(before)
+    assert selections == after
+
+    # Same test but ordered differently
+    before.reverse()
+    selections = Selection.reduce(before)
+    assert selections == after
+
+
+def test_selection_reduce7() -> None:
+    """Test reduce for consecutive merges."""
+    before = [Selection(0, 32), Selection(16, 32), Selection(8, 16), Selection(32, 32), Selection(96, 8)]
+    after = [Selection(0, 64), Selection(96, 8)]
+    selections = Selection.reduce(before)
+    assert selections == after
+
+    # Same test but ordered differently
+    before.reverse()
+    selections = Selection.reduce(before)
+    assert selections == after
